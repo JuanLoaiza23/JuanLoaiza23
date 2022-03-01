6fc4ef750b8cd2db457261595c53667e51a87a41
Juan Manuel Loaiza Perez <jmloaiza11@misena.edu.co>
2022-03-01 12:17:12 -0500

    Chore
    Se adjunta desarrollo reto 1

---
+++ Projects/Utest automation Selenium/TestUtest/src/main/java/web/PrincipalMenu.java
@@ -0,0 +1,113 @@
+package web;
+
+import org.openqa.selenium.*;
+import org.junit.*;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.util.concurrent.TimeUnit;
+
+public class PrincipalMenu {
+
+    private WebDriver webDriver;
+    private final int WAIT_SECONDS = 3000;
+
+    @Before
+    public void getIntoTestWeb() {
+        //Browser Options
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("start-maximized");
+
+        //Get Web page
+        System.setProperty("webdriver.chrome.driver", "src\\main\\resources\\chromedriver.exe");
+        webDriver = new ChromeDriver(options);
+        webDriver.get("https://www.utest.com");
+
+        WebElement logo = webDriver.findElement(By.xpath("//unauthenticated-header/div[1]/div[1]/a[1]/img[1]"));
+        WebDriverWait wait = new WebDriverWait(webDriver, WAIT_SECONDS);
+        wait.until(ExpectedConditions.elementToBeClickable(logo));
+        Assert.assertTrue(logo.isDisplayed());
+
+
+    }
+
+    @Test
+    public void validateOptions() {
+        //Options
+        WebElement buttonWhyUTest = webDriver.findElement(By.linkText("Why uTest"));
+        WebElement buttonAboutUs = webDriver.findElement(By.linkText("About Us"));
+        WebElement buttonProjects = webDriver.findElement(By.linkText("Projects"));
+        WebElement buttonLogin = webDriver.findElement(By.linkText("Log In"));
+        WebElement buttonContactUs = webDriver.findElement(By.linkText("Contact Us"));
+
+        Assert.assertTrue(buttonWhyUTest.isDisplayed());
+        Assert.assertTrue(buttonAboutUs.isDisplayed());
+        Assert.assertTrue(buttonProjects.isDisplayed());
+        Assert.assertTrue(buttonLogin.isDisplayed());
+        Assert.assertTrue(buttonContactUs.isDisplayed());
+    }
+
+    @Test
+    public void getIntoToOptionWhyUTest(){
+
+       // ((JavascriptExecutor)webDriver).executeScript("window.open()");
+        webDriver.findElement(By.linkText("Why uTest")).click();
+
+        WebElement text=  webDriver.findElement(By.className("why-utest__header-heading"));
+        WebDriverWait wait = new WebDriverWait(webDriver, WAIT_SECONDS);
+        wait.until(ExpectedConditions.elementToBeClickable(text));
+        Assert.assertEquals("Why uTest?",text.getAttribute("innerText"));
+    }
+    @Test
+    public void getIntoToOptionWhyAboutUs(){
+
+
+        webDriver.findElement(By.linkText("About Us"));
+        WebElement text=  webDriver.findElement(By.className("section-big-title-unauth"));
+        WebDriverWait wait = new WebDriverWait(webDriver, WAIT_SECONDS);
+        wait.until(ExpectedConditions.elementToBeClickable(text));
+        Assert.assertEquals("About Us",text.getAttribute("innerText"));
+    }
+    @Test
+    public void getIntoToOptionWhyProjects(){
+
+
+        webDriver.findElement(By.linkText("Log In")).click();
+
+        WebElement text=  webDriver.findElement(By.className("section-big-title-unauth"));
+        WebDriverWait wait = new WebDriverWait(webDriver, WAIT_SECONDS);
+        wait.until(ExpectedConditions.elementToBeClickable(text));
+        Assert.assertTrue(text.isEnabled());
+
+    }
+    @Test
+    public void getIntoToOptionWhyLogin(){
+
+        webDriver.findElement(By.linkText("Log In")).click();
+        WebElement button=  webDriver.findElement(By.className("btn"));
+        WebDriverWait wait = new WebDriverWait(webDriver, WAIT_SECONDS);
+        wait.until(ExpectedConditions.elementToBeClickable(button));
+        Assert.assertTrue(button.isEnabled());
+
+    }
+    @Test
+    public void buttonValidateBecomeUTester(){
+
+        webDriver.findElement(By.linkText("Become a uTester")).click();
+
+        webDriver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        WebElement inputName = webDriver.findElement(By.id("firstName"));
+        Assert.assertTrue(inputName.isDisplayed());
+    }
+
+
+    @After
+    public void closeBrowser(){
+        webDriver.close();
+    }
+
+
+}
